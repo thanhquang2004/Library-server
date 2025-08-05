116654e1a005dd1f660c83c3b7791d9334451a60
tintrung <huynhtrungtin1506@gmail.com>
2025-08-05 10:03:34 +0700

    build model payment

--- models/Payment.js
+++ models/Payment.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 
 const PaymentSchema = new mongoose.Schema({
+  paymentId: String,
   fine: { type: mongoose.Schema.Types.ObjectId, ref: "Fine", required: true },
   amount: { type: Number, required: true },
   method: { type: String, enum: ["cash", "credit", "online"], required: true },
   paidDate: { type: Date, default: Date.now },
+  transactionId: String
 });
 
 module.exports = mongoose.model("Payment", PaymentSchema);
