c71679f6bc75dd9fe0b1579efd6271a64e92fc9b
Huynh Trung Tin <116237224+tintrung1234@users.noreply.github.com>
2025-08-05 10:40:44 +0700

    delete bookitemId

--- models/BookItem.js
+++ models/BookItem.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const BookItemSchema = new mongoose.Schema({
-  bookItemId: String,
   book: { type: mongoose.Schema.Types.ObjectId, ref: 'Book' },
   barcode: String,
   isReferenceOnly: { type: Boolean, default: false },
